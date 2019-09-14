9c1f35ef85ec1b01e2bc0e71a53b58fdd628c782
Gregorius Ariyo <gregoriusemail30@gmail.com>
2019-09-14 15:23:40 +0700

    beautify

--- evote/settings.py
+++ evote/settings.py
@@ -120,13 +120,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-TATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+TATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 django_heroku.settings(locals())
 del DATABASES['default']['OPTIONS']['sslmode']
