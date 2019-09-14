42b1fb40ef0b69ffbd06c2543e71ee68034c7891
Gregorius Ariyo <gregoriusemail30@gmail.com>
2019-09-14 10:10:24 +0700

    heroku deploy

--- votes/admin.py
+++ votes/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Caketang, Pemilih
 
+class CaketangAdmin(admin.ModelAdmin):
+    pass
+
+class PemilihAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Caketang)
 admin.site.register(Pemilih)
 # Register your models here.
